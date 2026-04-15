51339e06634332ecf2186345b45802ddfa24755d
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-04-15 17:49:56 -0500

    test: add GET response tests

--- src/test/java/com/sergio/noteapp/NoteappApplicationTests.java
+++ src/test/java/com/sergio/noteapp/NoteappApplicationTests.java
@@ -1,13 +1,44 @@
 package com.sergio.noteapp;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NoteappApplicationTests {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
-	}
+    void shouldReturnANoteWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/notes/20", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(20);
+        String title = documentContext.read("$.title");
+        assertThat(title).isEqualTo("Note 20");
+        String content = documentContext.read("$.content");
+        assertThat(content).isEqualTo("Content 20");
+        String owner = documentContext.read("$.owner");
+        assertThat(owner).isEqualTo("sergio");
+    }
+
+    @Test
+    void shouldNotReturnANoteWhenIdIsUnknown() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/notes/2000", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
 
 }
